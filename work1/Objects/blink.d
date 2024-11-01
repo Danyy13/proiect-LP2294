a6c081de2bc7c787ec1ac6bc552b58cea1695f3b
Danyy13 <87762040+Danyy13@users.noreply.github.com>
2024-11-01 10:23:59 +0200

    Add files via upload

---
+++ work1/Objects/blink.d
@@ -0,0 +1,3 @@
+.\objects\blink.o: blink.c
+.\objects\blink.o: C:\Keil_v5\ARM\INC\Philips\lpc22xx.h
+.\objects\blink.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
--- work1/main.c
+++ work1/main.c
@@ -1,40 +1,69 @@
 #include <lpc22xx.h>
+#include <stdint.h>
 
-void LED_init() {
-	PINSEL1 &= ~(3 << 28); // setez pinul P30 sa fie GPIO
-	IO0DIR |= 1u << 30; // setez P30 sa fie output
+void setPinAsGPIO(uint8_t pin) {
+	if (pin & 0xF0) { // verificare daca pin >= 16
+		PINSEL1 &= ~(0b11 << ((pin - 16) / 2));
+		//PINSEL1 = 0x00000000; // seteaza toti pinii P0.X (X de la 16 la 31) sa fie GPIO
+	}
+	else {
+		PINSEL0 = ~(0b11 << (pin / 2));
+		//PINSEL0 = 0X00000000; // seteaza toti pinii P0.X (X de la 0 la 15) sa fie GPIO
+	}
 }
 
-void LED_on() {
-	IO0SET = 1 << 30; // dau semnal de HIGH
+void LED_init(uint8_t pin) {
+	setPinAsGPIO(pin); // seteaza pinul sa aiba functie de GPIO
+	IO0DIR |= 1u << pin; // setez pinul P0.[pin] sa fie output
 }
 
-void LED_off() {
-	IO0CLR = 1 << 30; // dau semnal de LOW
+void LED_on(uint8_t pin) {
+	IO0SET |= 1u << pin; // dau semnal de HIGH pe pinul pin
 }
 
-void Button_init() {
-	PINSEL0 &= ~3; // 14
-	IO0DIR &= ~1u; // set as input
+void LED_off(uint8_t pin) {
+	IO0CLR |= 1u << pin; // dau semnal de LOW
 }
 
-int Button_pressed() {
-	return (IO0PIN & 1) == 1;
+void LED_blink(uint8_t pin) {
+	LED_on(pin);
+	for (int i = 0;i < 1000000;i++); // delay
+	LED_off(pin);
+	for (int i = 0;i < 1000000;i++); // delay
 }
+
+//void Button_init(uint8_t pin) {
+//	setPinAsGPIO(pin);
+//	IO0DIR &= ~(1u << pin); // setez pinul P0.0 ca input
+//}
+//
+//int Button_pressed(uint8_t pin) {
+//	return (IO0PIN & (1u << pin)) == 0; // logca inversa
+//}
+//
+//void LED_buttonBlink(uint8_t ledPin, uint8_t buttonPin) {
+//	if(Button_pressed(buttonPin)) {
+//		for(i = 0; i < 10000; i++); // delay
+//		if(Button_pressed(buttonPin)) {
+//				LED_on(ledPin);
+//			} else {
+//				LED_off(ledPin);
+//			}
+//		}
+//	}
+//}
 
 int main(void)
 {
-	volatile unsigned long int i = 0;
-	LED_init();
-	Button_init();
+	uint8_t ledPin = 14;
+	//uint8_t buttonPin = 1;
+	
+	LED_init(ledPin);
+	//Button_init(buttonPin);
 	
 	while(1) {
-		if(Button_pressed()) {
-			for(i = 0; i < 1000; i++);
-			if(Button_pressed()) {
-				LED_on();
-			}
-		}
+		LED_blink(ledPin);
+		// LED_buttonBlink(pin);
 	}
 	
 	return 0;
